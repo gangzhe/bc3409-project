c8345b56082e18a6cf9497ea4c983024065ce825
gangzhe <60089644+gangzhe@users.noreply.github.com>
2022-11-09 15:26:46 +0800

    search and analyse

--- gui.py
+++ gui.py
@@ -19,7 +19,7 @@ def preprocess_tweet(tweet):
     wnl = WordNetLemmatizer()
 
     # lowercase
-    tweet = tweet.lower()
+    tweet = str(tweet).lower()
     # remove punctuation
     tweet = ''.join([c for c in tweet if c not in string.punctuation])
     # remove stopwords and apply stemming
@@ -29,7 +29,6 @@ def preprocess_tweet(tweet):
 
 
 def analyze_tweet():
-    global model
     tweet = text_tweet_analyzer.get("1.0", "end-1c")
     tweet = preprocess_tweet(tweet)
 
@@ -49,34 +48,63 @@ def scrape_latest_tweets():
             df = pd.read_csv('latest_tweets.csv')
             break
         except FileNotFoundError:
-            print('sleep')
+            print('Processing...')
             time.sleep(5)
 
     df["preprocessed_tweet"] = df["Cleaned Tweet"].apply(preprocess_tweet)
     X = bert.encode(df["preprocessed_tweet"])
-    mean_score = np.mean(model.predict_proba(X))
+    # mean_score = np.mean(model.predict_proba(X))
     mean_score = np.mean(model.predict(X))
     sentiment = 'Bull' if mean_score >= 0.5 else 'Bear'
 
     label_latest_result['text'] = f'{mean_score*100:.2f}% bullish => {sentiment} Market'
 
-    
+
+def search():
+    query = text_search.get("1.0", "end-1c")
+    command = f'python search.py --query {query}'
+    os.system(f'start cmd /k "{command}"')
+
+    while True:
+        try:
+            df = pd.read_csv(f'search_{query}.csv')
+            break
+        except FileNotFoundError:
+            print('Processing...')
+            time.sleep(5)
+
+    df["preprocessed_tweet"] = df["Cleaned Tweet"].apply(preprocess_tweet)
+    X = bert.encode(df["preprocessed_tweet"])
+    # mean_score = np.mean(model.predict_proba(X))
+    mean_score = np.mean(model.predict(X))
+    sentiment = 'Bull' if mean_score >= 0.5 else 'Bear'
+
+    label_search_result['text'] = f'{mean_score*100:.2f}% bullish => {sentiment} Market'
+
+
 
 root = Tk()
 root.title('Stock Tweets Analyzer GUI')
 root.geometry('500x500')
 root.resizable(False, False)
 
-label_tweet_analyzer = Label(root, text="Enter tweet to analyze").place(x=200, y=25)
+label_tweet_analyzer = Label(root, text="Enter tweet to analyze").place(x=180, y=25)
 text_tweet_analyzer = Text(root, height=3, width=50)
 text_tweet_analyzer.place(x=50, y=50)
-button_tweet_analyzer = Button(root, text='Analyze Tweet', command=lambda:analyze_tweet()).place(x=205, y=110)
+button_tweet_analyzer = Button(root, text='Analyze Tweet', command=lambda:analyze_tweet()).place(x=190, y=110)
 label_tweet_analyzer_result = Label(root)
-label_tweet_analyzer_result.place(x=220, y=145)
+label_tweet_analyzer_result.place(x=190, y=145)
 
 label_latest = Label(root, text='Analyse latest tweets (will take some time)').place(x=125, y=225)
 button_latest = Button(root, text='Analyse', command=scrape_latest_tweets).place(x=210, y=250)
 label_latest_result = Label(root)
 label_latest_result.place(x=150, y=285)
+
+label_search = Label(root, text='Search a stock or company').place(x=170, y=350)
+text_search = Text(root, height=3, width=50)
+text_search.place(x=50, y=375)
+button_search = Button(root, text='Analyse', command=search).place(x=210, y=435)
+label_search_result = Label(root)
+label_search_result.place(x=150, y=470)
 
 root.mainloop()
---
+++ search.py
@@ -0,0 +1,101 @@
+from datetime import datetime
+import tweepy
+import pandas as pd
+import time
+import csv
+import preprocessor as p
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--query', type=str, required=True)
+args = parser.parse_args()
+
+
+# Twitter Authorization
+# Read keys.csv for keys
+with open('keys.csv', mode='r') as infile:
+    reader = csv.reader(infile)
+    KEYS = {rows[0]:rows[1] for rows in reader}  
+
+consumer_key = KEYS['consumer_key']
+consumer_secret = KEYS['consumer_secret']
+bearer_token = KEYS['bearer_token']
+access_key = KEYS['access_key']
+access_secret = KEYS['access_secret']
+# Pass twitter credentials to tweepy
+auth = tweepy.OAuth2BearerHandler(bearer_token)
+api = tweepy.API(auth)
+
+
+
+# Helper function to extract more information from twweettext
+p.set_options(p.OPT.URL, p.OPT.MENTION, p.OPT.RESERVED, p.OPT.EMOJI, p.OPT.SMILEY)
+def processTweetText(tweettext):
+    parsed_tweet = p.parse(tweettext)
+    tweeturls = None
+    tweethashtags = None
+    tweetmentions = None
+    
+    if(parsed_tweet.urls):
+        tweeturls = [url.match for url in parsed_tweet.urls]
+    if(parsed_tweet.hashtags):
+        tweethashtags = [ht.match for ht in parsed_tweet.hashtags]
+    if(parsed_tweet.mentions):
+        tweetmentions = [m.match for m in parsed_tweet.mentions]
+    
+    tweettextcleaned = p.clean(tweettext).replace("#", "").replace(":", "")
+    return [tweeturls, tweethashtags, tweetmentions, tweettextcleaned]
+
+
+
+def scrapeTwitter(q, df, count, numTweets, seen):
+    searchResults = tweepy.Cursor(
+        api.search_tweets,
+        q,
+        lang='en',
+        result_type='mixed',
+        tweet_mode='extended',
+        count=100,
+    ).items(count)
+
+    statusList = [status for status in searchResults]
+    for status in statusList:
+        _, _, _, tweetTextCleaned = processTweetText(status.full_text)
+        
+        if tweetTextCleaned in seen: # prevent duplicates
+            continue
+
+        seen.add(tweetTextCleaned)
+        df.loc[len(df)] = [tweetTextCleaned]
+        numTweets += 1
+
+    print(f"Query: {q}\t\tTotal scraped: {numTweets}")
+    return numTweets
+
+
+
+df = pd.DataFrame(columns=['Cleaned Tweet'])
+numTweets = 0
+count = 1000
+q = args.query
+seen = set()
+
+
+try:
+    numTweets = scrapeTwitter(q, df, count, numTweets, seen)
+except Exception as e:
+    print(e)
+    print("Time to take a 15 minutes break...")
+    time.sleep(15*60)
+    print("Resume scraping...")
+    numTweets = scrapeTwitter(q, df, count, numTweets, seen)
+
+
+
+timestamp = datetime.today().strftime('%Y%m%d_%H%M%S')
+# filename_csv = 'tweets_' + str(numTweets) + '_' + timestamp + '.csv'
+filename_csv = f'search_{args.query}.csv'
+df.to_csv(filename_csv, index=False)
+print("File saved as: ", filename_csv)
+
